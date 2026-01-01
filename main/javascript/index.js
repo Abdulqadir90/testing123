2f95237cd80490e3dea5a34bbf942f00ae83c0c8
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-01-01 21:09:05 +0000

    testing out some string concatenation and lengths

---
+++ main/javascript/index.js
@@ -0,0 +1,12 @@
+
+// string concatenation 
+
+let name = "Abdulqadir";
+let message = "Hello";
+
+console.log(message + " there " + name);
+
+// string lengths, done this via the developer tools
+var tweet = prompt("Compose your tweet");
+var tweetCount = tweet.length;
+alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining");
