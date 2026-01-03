31c64efe722f178d08462a049e5038c8ede0581a
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-01-03 17:05:03 +0000

    simplified things by adding 3 variable for each color making things easier to understand

--- main/DOM/index.js
+++ main/DOM/index.js
@@ -1,7 +1,15 @@
 
 // variable called body getting the body element/node in that tree
 let body = document.querySelector('body');
-// telling the document to select the element id red and listen to the click event
-document.querySelector('#red').addEventListener('click', function(){
+
+// created 3 seperate variables finding the button with id red,green,blue and storing them
+let redButton = document.querySelector('#red');
+let greenButton = document.querySelector('#green');
+let blueButton = document.querySelector('#blue');
+// when the red button is clicked run the code inside this function
+redButton.addEventListener('click', function(){
+// changing the page background to red
 body.style.backgroundColor = 'red';
-})
+});
+
+
