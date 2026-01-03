47eb5d3ce19e586243a156428d816eacebc5a6a3
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-01-03 20:02:09 +0000

    completed rgb buttos and background color change logic

--- main/DOM/index.js
+++ main/DOM/index.js
@@ -1,5 +1,5 @@
 
-// variable called body getting the body element/node in that tree
+// variable called body finding the body element/node in that tree and store it in a variable called 'body'
 let body = document.querySelector('body');
 
 // created 3 seperate variables finding the button with id red,green,blue and storing them
@@ -10,6 +10,11 @@ let blueButton = document.querySelector('#blue');
 redButton.addEventListener('click', function(){
 // changing the page background to red
 body.style.backgroundColor = 'red';
+}); // ends the function
+
+greenButton.addEventListener('click', function(){
+body.style.backgroundColor = 'green';
 });
-
-
+blueButton.addEventListener('click', function(){
+    body.style.backgroundColor = 'blue';
+});
